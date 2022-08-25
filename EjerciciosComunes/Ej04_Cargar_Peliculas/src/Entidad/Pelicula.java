7e8936b80f69a14641e5e9ecc772e73b0bad314f
Fede Trucco <103152265+truquinio@users.noreply.github.com>
2022-08-25 17:26:50 -0300

    25/08/2022

--- EjerciciosComunes/Ej04_Cargar_Peliculas/src/Entidad/Pelicula.java
+++ EjerciciosComunes/Ej04_Cargar_Peliculas/src/Entidad/Pelicula.java
@@ -1,7 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Un cine necesita implementar un sistema en el que se puedan cargar peliculas. Para esto, 
+tendremos una clase Pelicula con el título, director y duración de la película (en horas). 
+Implemente las clases y métodos necesarios para esta situación, teniendo en cuenta lo 
+que se pide a continuación:
+
+En el servicio deberemos tener un bucle que crea un objeto Pelicula pidiéndole al usuario 
+todos sus datos y guardándolos en el objeto Pelicula.
+Después, esa Pelicula se guarda una lista de Peliculas y se le pregunta al usuario si quiere 
+crear otra Pelicula o no.
+
+Después de ese bucle realizaremos las siguientes acciones: 
+
+•  Mostrar en pantalla todas las películas.
+•  Mostrar en pantalla todas las películas con una duración mayor a 1 hora.
+•  Ordenar las películas de acuerdo a su duración (de mayor a menor) y mostrarlo en 
+pantalla.
+•  Ordenar las películas de acuerdo a su duración (de menor a mayor) y mostrarlo en 
+pantalla.
+•  Ordenar las películas por título, alfabéticamente y mostrarlo en pantalla.
+•  Ordenar las películas por director, alfabéticamente y mostrarlo en pantalla.
  */
 package Entidad;
 
@@ -45,7 +62,6 @@ public class Pelicula {
 
     @Override
     public String toString() {
-        return "Titulo: " + titulo + ", Director:" + director + ", Duracion=" + duracion;
+        return "Titulo: " + titulo + ", Director: " + director + ", Duración: " + duracion + " hr.";
     }
-    
 }
